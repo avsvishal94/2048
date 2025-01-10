3bb417832677d8904f0ff3803b7c99a1d3152a12
avsvishal94 <61268640+avsvishal94@users.noreply.github.com>
2025-01-10 21:47:54 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-sudo su
-yum install git -y
-yum install httpd -y
-git clone https://github.com/gabrielecirulli/2048.git
-cd /home/ec2-user/2048
+sudo yum install httpd -y
+cd 2048
 sudo cp -r * /var/www/html/
-sudo systemctl enable httpd
-sudo systemctl start httpd
+sudo systemctl enable httpd.service
+sudo systemctl start httpd.service
